b1b4c2dc653780eb3b94afae66a00021e9b6c871
MO99ME99 <m.m.hosseini1099@gmail.com>
2023-04-11 10:32:17 -0400

    add KhaneyeDoost to Project

---
+++ src/KhaneyeDoost.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class KhaneyeDoost {
+    public class FriendsHouse{
+        public static void main(String args[]){
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+            System.out.print(input*input);
+        }
+    }
+}
