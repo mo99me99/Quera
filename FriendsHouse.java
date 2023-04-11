579997436b695038a83b33765978588a19cd235b
MO99ME99 <m.m.hosseini1099@gmail.com>
2023-04-11 10:15:18 -0400

    add FriendsHouse

---
+++ FriendsHouse.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class FriendsHouse{
+	public static void main(String args[]){
+	Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		System.out.print(input*input);
+	}
+}
